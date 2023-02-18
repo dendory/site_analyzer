910a3c5c277676bcf384f69ed6b16dd5f1d40aab
Patrick Lambert <elfguy@dendory.net>
2023-02-18 11:31:17 -0500

    Update front_end.py

--- front_end/front_end.py
+++ front_end/front_end.py
@@ -3,12 +3,13 @@ import connix
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
 @app.route('/api/analyze', methods=['POST'])
 	site = request.form['site']
+	return "Success."
 
 if __name__ == '__main__':
 	app.run(ssl_context='adhoc')
